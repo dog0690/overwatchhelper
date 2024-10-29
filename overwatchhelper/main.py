bc56a9cfaa8c9a82f8276c0b248136a961581269
Charley <charleymaivn@gmail.com>
2024-10-29 11:09:41 -0400

    names of all the characthers

---
+++ overwatchhelper/main.py
@@ -0,0 +1,48 @@
+
+#Tanks
+T1 = 'Dva'
+T2 = 'Doomfist'
+T3 = 'JunkerQueen'
+T4 = 'Mauga'
+T5 = 'Ramatra'
+T6 = 'Reinhardt'
+T7 = 'Roadhog'
+T8 = 'Sigma'
+T9 = 'Winston'
+T10 = 'WreckingBall'
+T11 = 'Zarya'
+
+
+#Damage
+
+A1 = 'Ashe'
+A2 = 'Bastion'
+A3 = 'Cassidy'
+A4 = 'Echo'
+A5 = 'Genji'
+A6 = 'Hanzo'
+A7 = 'Junkrat'
+A8 = 'Mei'
+A9 = 'Pharah'
+A10 = 'Reaper'
+A11 = 'Sojourn'
+A12 = 'Soldier76'
+A13 = 'Sombra'
+A14 = 'Symmetra'
+A15 = 'Tobjorn'
+A16 = 'Tracer'
+A17 = 'Venture'
+A18 = 'Widowmaker'
+
+#Support
+S1 = 'Ana'
+S2 = 'Baptiste'
+S3 = 'Brigitte'
+S4 = 'Illari'
+S5 = 'Juno'
+S6 = 'Kiriko'
+S7 = 'Lifeweaver'
+S8 = 'Lucio'
+S9 = 'Mercy'
+S10 = 'Moira'
+S11 = 'Zenyatta'
